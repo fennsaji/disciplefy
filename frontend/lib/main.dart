9dabc8ca65b202b61dab593aefb78d43394a159f
Fenn Saji <fenn@swnglobal.com>
2025-11-28 16:57:35 +0530

    Google Fonts Bundling

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'core/config/app_config.dart';
 import 'core/di/injection_container.dart';
 import 'features/daily_verse/data/services/daily_verse_cache_interface.dart';
@@ -83,6 +84,9 @@ void main() async {
   }
 
   try {
+    // Disable Google Fonts runtime fetching - use bundled fonts for faster loading
+    GoogleFonts.config.allowRuntimeFetching = false;
+
     // Initialize Hive for local storage
     await Hive.initFlutter();
 
